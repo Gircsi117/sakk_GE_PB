b3daa9b7a1e5b968b515e2c44961f6b02033d6d2
Gircsi117 <gilianerik1107@gmail.com>
2021-12-13 11:06:12 +0100

    Menu bar létrehozása

--- sakk_GE_PB/Form1.cs
+++ sakk_GE_PB/Form1.cs
@@ -16,5 +16,15 @@ namespace sakk_GE_PB
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
